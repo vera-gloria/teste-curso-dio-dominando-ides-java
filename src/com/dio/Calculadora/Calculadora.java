ba0516bc2fe8e7be6484aabd49d2ddf5d30cbadf
vera-gloria <twiiggi@gmail.com>
2022-03-16 22:15:42 -0300

    Criação da Calculadora

---
+++ src/com/dio/Calculadora/Calculadora.java
@@ -0,0 +1,43 @@
+package com.dio.Calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		double a, b;
+		
+		System.out.println("Digite o primeiro valor: ");		
+		a = scan.nextInt();
+		System.out.println("Digite o segundo valor: ");
+		b = scan.nextInt();
+		
+		double soma = soma(a,b);
+		double subtracao = subtracao(a,b);
+		double multiplicacao = multiplicacao(a,b);
+		double divisao = divisao(a,b);
+		
+		System.out.println("soma " + soma);
+		System.out.println("subtração " + subtracao);
+		System.out.println("multiplicação " + multiplicacao);
+		System.out.println("divisão " + divisao);
+	}
+	
+	private static double soma(double a, double b) {
+		return a + b;
+	}
+	
+	private static double subtracao(double a, double b) {
+		return a - b;
+	}
+	
+	private static double multiplicacao(double a, double b) {
+		return a * b;
+	}
+	
+	private static double divisao(double a, double b) {
+		return a / b;
+	}
+ }
